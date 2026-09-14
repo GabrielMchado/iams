f9187753760c2573d9103f5a76d3fd35040e1dc7
GabrielMchado <bieloliveiramachado@gmail.com>
2026-09-14 20:56:49 -0300

    Remove chave JWT hardcoded, usa variável de ambiente

--- src/main/java/com/gabrielmachado/iams/service/JwtService.java
+++ src/main/java/com/gabrielmachado/iams/service/JwtService.java
@@ -1,13 +1,33 @@
 package com.gabrielmachado.iams.service;
 
 import com.gabrielmachado.iams.model.UserModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
-    public String generateToken(UserModel userModel){
-        return "OK";
+    private final SecretKey secretKey;
+
+    public JwtService(@Value("${jwt.secret}") String jwtSecret) {
+        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
     }
 
+    public String generateToken(UserModel userModel) {
+        long agora = System.currentTimeMillis();
+        long expiracao = agora + 60; // 1 hora
+
+        return Jwts.builder()
+                .subject(userModel.getEmail())
+                .claim("id", userModel.getId().toString())
+                .issuedAt(new Date(agora))
+                .expiration(new Date(expiracao))
+                .signWith(secretKey)
+                .compact();
+    }
 }
